227fbd31524a0dfe873d0d690db700aa3d2f76da
gilper0x <gilper0x@gmail.com>
2025-10-08 09:00:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,12 @@ if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
-rm -f cf
+
 rm -f install-tool.sh
+rm -f install-xray.sh
+rm -f install-menu.sh
+rm -f log-install.txt
+
 # --- Colors ---
 red='\e[1;31m'
 green='\e[0;32m'
